c59ef56c155db07f2c7f222b38c0e68335f14d2d
David Lee <David Lee>
2016-02-02 21:35:07 -0700

    update DB and added scripture file

--- DBconnect.php
+++ DBconnect.php
@@ -17,3 +17,4 @@ $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $dbName = 'php';
 echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n"; 
 $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+?>
---
+++ scriptures.php
@@ -0,0 +1,27 @@
+<? require ('DBconnect.php') ?>
+<html>
+  <head>
+    <title>Week 05 : Team Activity</title>
+  </head>
+  <body>
+    <?php   
+      $query =  mysql_query("SELECT * FROM scriptures");
+	  echo "<table><tr><td>First Name</td><td>Last Name</td></tr>";
+
+		while($row = mysql_fetch_array($query))
+		{
+			echo "<tr><td>";
+			echo $row['book'];
+			echo "</td><td>";
+			echo $row['chapter'];
+			echo "<tr><td>";
+			echo $row['verse'];
+			echo "<tr><td>";
+			echo $row['content'];
+			echo "</td></tr>";
+		}
+
+echo "</table>";
+    ?>
+  </body>
+</html>
