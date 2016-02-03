36c03b61f13574e310bec6e21e55dc8edf5cf1f4
David Lee <David Lee>
2016-02-02 20:54:58 -0700

    updated db name

--- DBconnect.php
+++ DBconnect.php
@@ -3,12 +3,12 @@
 function loadDatabase()
 {
 
-  $dbHost = "192.168.1.17";
-  $dbPort = "80";
+  $dbHost = "127.13.131.2";
+  $dbPort = "3306";
   $dbUser = "adminbRsxs9S";
   $dbPassword = "D13liCk-KxN3";
 
-     $dbName = "testdb";
+     $dbName = "php";
 
      $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
@@ -21,14 +21,14 @@ function loadDatabase()
      else
      {
           // In the openshift environment
-          //echo "Using openshift credentials: ";
+          echo "Using openshift credentials: ";
 
           $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
           $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
           $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
           $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
      }
-     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+     echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 
      $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
