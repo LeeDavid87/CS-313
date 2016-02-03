37c6a59ebab2437b72b4a6eb5de19ed6518d6905
David Lee <David Lee>
2016-02-02 21:00:34 -0700

    DBupdate

--- DBconnect.php
+++ DBconnect.php
@@ -1,39 +1,19 @@
 <?php
-
-function loadDatabase()
+try
 {
-
-  $dbHost = "127.13.131.2";
-  $dbPort = "3306";
-  $dbUser = "adminbRsxs9S";
-  $dbPassword = "D13liCk-KxN3";
-
-     $dbName = "php";
-
-     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-
-     if ($openShiftVar === null || $openShiftVar == "")
-     {
-          // Not in the openshift environment
-          //echo "Using local credentials: ";
-          require("setLocalDatabaseCredentials.php");
-     }
-     else
-     {
-          // In the openshift environment
-          echo "Using openshift credentials: ";
-
-          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-     }
-     echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
-
-     $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-     return $db;
-
+  $User = "adminbRsxs9S";
+  $Password = "D13liCk-KxN3";
+   $db = new PDO('mysql:host=127.13.131.2;dbname=polls_db', $user, $password);
 }
-
-?>
+catch (PDOException $ex) 
+{
+   echo 'Error!: ' . $ex->getMessage();
+   die(); 
+}
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = 'php';
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n"; 
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
