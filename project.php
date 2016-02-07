4a116783c3d09dc6e4464e28773ed621bef47dfe
David Lee <David Lee>
2016-02-07 15:56:35 -0700

    edit

--- project.php
+++ project.php
@@ -6,19 +6,17 @@
   <? php 
     $queen =  $db->query("SELECT * FROM queen");
 	$stud =  $db->query("SELECT * FROM stud");
+	?>
     <div id="stud">
-	  foreach ($stud as $row)
+	  <?php 
+	  	  foreach ($stud as $row)
 		{
 			echo '<div> Name: '. $row['name'] . ' </br> ' . 
 			'Picture ' . $row['stud_picture'] .  '</div>';
 		}
+		?>
 	</div>
     <div id="queen">
-	  foreach ($queen as $row)
-		{
-			echo '<div> Name: '. $row['name'] . ' </br> ' . 
-			'Picture ' . $row['queen_picture'] .  '</div>';
-		}
 	</div>
     <div id="kitten">
 
@@ -29,5 +27,4 @@
     <div id="litter">
 
 	</div>
-  ?>
 <? require ('footer.php') ?>
