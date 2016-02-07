677e2bb1b1d181e0ff2c31cd99268fcae67582f7
David Lee <David Lee>
2016-02-07 15:05:59 -0700

    new files

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8" />
+  <title></title>
+  <link href="styleSheet.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+  <div id="siteContainer">
+    <div id="navBar">
+      <nav>
+        <div class="container">
+          <ul class="navbarleft" id="navBarLink">
+            <li class="navlink"><a class="navlink" href="index.html">Home</a></li>
+            <li class="navlink"><a class="navlink" href="#about">About</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  <div id="formHolder">
---
+++ mainPage.php
@@ -0,0 +1,11 @@
+<? require ('DBconnect.php') ?>
+<? require ('header.php') ?>
+  <div>
+    <span></span>
+  </div>
+  <div id="stud"></div>
+  <div id="queen"></div>
+  <div id="kitten"></div>
+  <div id="customer"></div>
+  <div id="litter"></div>
+<? require ('footer.php') ?>
