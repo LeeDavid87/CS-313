4831d10e690e6942110826b3640a4cc0a46197cb
David Lee <David Lee>
2016-02-07 15:23:07 -0700

    Updated php files

--- DBconnect.php
+++ DBconnect.php
@@ -15,6 +15,5 @@ $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
 $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $dbName = 'php';
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n"; 
 $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 ?>
