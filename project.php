888e2b2697d12afdf2cf7abdd7a2553a1f703f9f
David Lee <David Lee>
2016-02-07 15:49:05 -0700

    update database

--- project.php
+++ project.php
@@ -3,9 +3,43 @@
   <div>
     <span></span>
   </div>
-  <div id="stud"></div>
-  <div id="queen"></div>
-  <div id="kitten"></div>
-  <div id="customer"></div>
-  <div id="litter"></div>
+  <? php 
+    $queen =  $db->query("SELECT * FROM scriptures");
+	$stud =  $db->query("SELECT * FROM scriptures");
+	$litter =  $db->query("SELECT * FROM scriptures");
+	$kitten =  $db->query("SELECT * FROM scriptures");
+    <div id="stud">
+	  foreach ($stud as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ' . 
+			'Picture ' . $row['stud_picture'] .  '</div>';
+		}
+	</div>
+    <div id="queen">
+	  foreach ($queen as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ' . 
+			'Picture ' . $row['queen_picture'] .  '</div>';
+		}
+	</div>
+    <div id="kitten">
+	  foreach ($kitten as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ' . 
+			'Picture ' . $row['kitten_picture'] .  '</div>';
+		}
+	</div>
+    <div id="customer">
+	  foreach ($customer as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ';
+		}
+	</div>
+    <div id="litter">
+	  foreach ($litter as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ';
+		}
+	</div>
+  ?>
 <? require ('footer.php') ?>
