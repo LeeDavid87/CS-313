2468ca8af37dae05bf1e6d2e512f8b707cdc7ef7
David Lee <David Lee>
2016-02-07 18:41:13 -0700

    as

--- project.php
+++ project.php
@@ -1,27 +1,33 @@
 <? require ('DBconnect.php') ?>
 <? require ('header.php') ?>
-  <?php 
-    $query =  $db->query("SELECT * FROM scriptures");
-	?>
   <div>
     <span></span>
   </div>
-
+  <?php 
+    $queen =  $db->query("SELECT * FROM queen");
+	$stud =  $db->query("SELECT * FROM stud");
     <div id="stud">
+	  foreach ($stud as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ' . 
+			'Picture ' . $row['stud_picture'] .  '</div>';
+		}
 	</div>
-	
     <div id="queen">
+	  foreach ($queen as $row)
+		{
+			echo '<div> Name: '. $row['name'] . ' </br> ' . 
+			'Picture ' . $row['queen_picture'] .  '</div>';
+		}
 	</div>
-	
     <div id="kitten">
 
 	</div>
-	
     <div id="customer">
 
 	</div>
-	
     <div id="litter">
-	
+
 	</div>
+  ?>
 <? require ('footer.php') ?>
